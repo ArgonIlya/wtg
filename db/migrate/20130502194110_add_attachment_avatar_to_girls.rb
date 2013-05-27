cb49e29648169a9b05a48ed660446bfd55401e8a
Ilya Lukyanov <illukyanov@gmail.com>
2013-05-27 11:31:53 +0600

    some fixes

--- db/migrate/20130502194110_add_attachment_avatar_to_girls.rb
+++
@@ -1,5 +0,0 @@
-class AddAttachmentAvatarToGirls < ActiveRecord::Migration
-  def change
-    add_attachment :girls, :avatar
-  end
-end
--- db/migrate/20130514062557_add_answers_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddAnswersToUsers < ActiveRecord::Migration
-  def change
-  	add_column :users, :right, :int, default: 0
-  	add_column :users, :wrong, :int, default: 0
-  end
-end
