dcafbc99f1c4988556cdb9c17de72ef88a705892
Ilya Lukyanov <illukyanov@gmail.com>
2013-05-27 12:18:45 +0600

    some fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,5 @@ Wtg::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'http://who-that-girl.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'who-that-girl.herokuapp.com' }
 end
